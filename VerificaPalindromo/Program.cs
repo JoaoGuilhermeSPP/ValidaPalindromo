7ad17cbf8b4eb611f52f5f6d0fcdf8a496681000
JoaoGuilhermeSPP <joaoguipinheiro@gmail.com>
2026-05-12 10:41:11 -0300

    Codigo completo referente ao validador de palindromo

---
+++ VerificaPalindromo/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace VerificaPalindromo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diige uma palavra ou frase:");
+            string entrada = Console.ReadLine();
+            string textoIncluso = "";
+
+            foreach (char c in entrada)
+            {
+                if (char.IsLetterOrDigit(c)) // Verificando se é letra ou decimal
+                {
+                    textoIncluso += char.ToLower(c);
+                }
+            }
+
+            bool isPalindromo = true;
+            int medida = textoIncluso.Length;
+
+            for (int i = 0; i < medida / 2; i++)
+            {
+                if (textoIncluso[i] != textoIncluso[medida - 1 - i])//Verifico o inicio da palavra se é diferente do final
+                {
+                    isPalindromo = false;
+                    break;  //caso venha a falhar em um, eu não preciso ler os demais
+                }
+            }
+            if (isPalindromo) 
+            {
+                Console.WriteLine("É um palindromo!");
+            }
+            else
+            {
+                Console.WriteLine("Não é um palindromo");
+            }
+        }
+       
+    }
+}
